c759104bf63ac880433d6a1a23508eb5c167cb1e
soli92 <45817292+soli92@users.noreply.github.com>
2026-03-22 14:04:18 +0100

    Add Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#3b82f6",
+          foreground: "#ffffff",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
